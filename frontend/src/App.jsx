f95a26c7e48fd873dbc45d4665f5fa3ed97cea9a
Aditya-Mishra20 <adi.aditya2023@gmail.com>
2024-11-09 07:07:02 +0530

    ⭐feat: Implement lazy loading of pages using Suspense and dynamic importing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
 
-import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
+//Dynamic Importing - for lazy loading fetches pages only on demand
+const Home = lazy(() => import("./pages/Home"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   );
