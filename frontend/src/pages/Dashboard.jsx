e17d282c4be89b3afd787b2724011e30cf86a83d
Aditya-Mishra20 <adi.aditya2023@gmail.com>
2024-11-13 22:00:01 +0530

    🔄️refactor: Refactored layout styling in Dashboard component.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,11 +5,11 @@ import SideBar from "../components/SideBar";
 const Dashboard = () => {
   return (
     <main className=" h-screen w-screen flex items-center">
-      <section className=" h-full w-1/5 bg-slate-600 ">
+      <section className=" h-full w-1/5  border-r-2  ">
       <SideBar/>
       </section>
-      <section className=" h-full w-4/5 bg-orange-200 ">
-      <Navbar/>
+      <section className=" h-full w-4/5 ">
+
       </section>
     </main>
   );
