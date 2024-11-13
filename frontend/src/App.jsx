0b947694571dadc457a28c33ea48caa4431108db
Aditya-Mishra20 <adi.aditya2023@gmail.com>
2024-11-13 21:58:07 +0530

    ⭐feat: Added Navbar component and implemented Sign In and Login pages in App.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Loader from "./components/Loader";
+import Navbar from "./components/Navbar";
+import SignIn from "./pages/SignIn";
+import Login from "./pages/Login";
 
 //Dynamic Importing - for lazy loading fetches pages only on demand
 const Home = lazy(() => import("./pages/Home"));
@@ -11,11 +14,14 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
         <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/signup" element={<SignIn/>} />
+            <Route path="/login" element={<Login/>} />
           </Routes>
         </Suspense>
       </BrowserRouter>
