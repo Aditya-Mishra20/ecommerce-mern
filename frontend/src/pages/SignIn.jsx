3ae7564dd2fb1d394512af24dc10aae9591b15c7
Aditya-Mishra20 <adi.aditya2023@gmail.com>
2024-11-13 21:56:41 +0530

    ⭐feat: Add Login page component with sign-in form.

---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [state, setState] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setState((prevProps) => ({
+      ...prevProps,
+      [name]: value
+    }));
+    console.log(event.target);
+    
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(state);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={state.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={state.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+// export default SignIn;
