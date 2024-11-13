25fe50e4e02430751c825e0485b1a3e1fd7e48d5
Aditya-Mishra20 <adi.aditya2023@gmail.com>
2024-11-13 21:56:16 +0530

    ⭐refactor: Update Navbar component for dynamic rendering of user-related elements.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,17 +3,44 @@ import { NavLink } from "react-router-dom";
 import { FaBell, FaHeart, FaSearch, FaUserCircle } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 
+const user = {
+  id: "dee",
+  avatar: "",
+  isAdmin: true,
+};
+
 const Navbar = () => {
   return (
-    <nav className=" w-full h-16 bg-slate-700 flex justify-between items-center">
+    <nav className=" w-full h-16 p-2 flex justify-between items-center border-b-2">
       <h3 className="text-2xl">Logo</h3>
       <div className="flex items-center gap-3 justify-center ">
-        <input type="text" placeholder="Search..." />
-        <FaSearch />
-        <FaBell />
-        <FaUserCircle />
-        <FaHeart />
-        <FaCartShopping />
+        <NavLink to="search">
+          <FaSearch />
+        </NavLink>
+        {user?.id ? (
+          <>
+            <NavLink>
+              <NavLink to="/notification">
+                <FaBell />
+              </NavLink>
+            </NavLink>
+            {user?.avatar ? (
+              <img src={user.avatar} alt="avatar" />
+            ) : (
+              <NavLink to="/profile">
+                <FaUserCircle />
+              </NavLink>
+            )}
+            <NavLink to="wishlist">
+              <FaHeart />
+            </NavLink>
+            <NavLink to="/cart">
+              <FaCartShopping />
+            </NavLink>
+          </>
+        ) : (
+          <NavLink to="/signin">Sign In</NavLink>
+        )}
       </div>
     </nav>
   );
