0fcd247a9d86ac05ed7ba0c243ff2ec1179a1d79
Aditya-Mishra20 <adi.aditya2023@gmail.com>
2024-11-14 23:48:37 +0530

    ⭐feat: connecting frontend to backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server:{
+    proxy:{
+      "/api" : "http://localhost:8000"
+    }
+  },
   plugins: [react()],
 });
