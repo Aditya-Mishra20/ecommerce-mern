6a13a4cb13a3e9103044ab753f1a7c8d857af05d
Aditya-Mishra20 <adi.aditya2023@gmail.com>
2024-11-16 19:54:59 +0530

    🔄️refactor: Update user ID and isAdmin status, Navbar styling, admin dashboard link.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,16 +4,16 @@ import { FaBell, FaHeart, FaSearch, FaUserCircle } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 
 const user = {
-  id: "dee",
+  id: "as",
   avatar: "",
-  isAdmin: true,
+  isAdmin: false,
 };
 
 const Navbar = () => {
   return (
-    <nav className=" w-full h-16 p-2 flex justify-between items-center border-b-2">
+    <nav className=" w-screen h-16 p-2 flex bg-slate-400 justify-around items-center border-b-2">
       <h3 className="text-2xl">Logo</h3>
-      <div className="flex items-center gap-3 justify-center ">
+      <div className="flex items-center gap-5 justify-center ">
         <NavLink to="search">
           <FaSearch />
         </NavLink>
@@ -37,6 +37,11 @@ const Navbar = () => {
             <NavLink to="/cart">
               <FaCartShopping />
             </NavLink>
+            {user?.isAdmin && (
+              <NavLink to="/admin/dashboard">
+                <button>Dash</button>
+              </NavLink>
+            )}
           </>
         ) : (
           <NavLink to="/signin">Sign In</NavLink>
