0564deaf0a92a3939b75ecc0d6ce685d6ca963ad
Aditya-Mishra20 <adi.aditya2023@gmail.com>
2024-11-21 22:09:20 +0530

    🔄️refactor: Refactored user route for authentication and file upload.

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,18 +1,18 @@
 import { Router } from "express";
-import { loginController, logoutController, registerController } from "../controllers/user.controller.js";
+import {
+  loginController,
+  logoutController,
+  registerController,
+} from "../controllers/user.controller.js";
 import upload from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-
 const router = Router();
 
-
 router.route("/register").post(upload.single("avatar"), registerController);
-router.route("/login").post(loginController)
+router.route("/login").post(loginController);
 
-
-
-// secured routes   
-router.route("/logout").post(verifyJWT, logoutController)
+// secured routes
+router.route("/logout").post(verifyJWT, logoutController);
 
 export default router;
